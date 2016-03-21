851306f56f20bb553f59db9fcf2cbcbb3ebe17e5
Sándor Koppándi <ifjsandor.koppandi@gmail.com>
2016-03-21 11:38:15 +0100

    Main

---
+++ main.h
@@ -0,0 +1,6 @@
+struct Piece
+{
+    char Position;  //Couleur = N ou B ou ' ' (s'il n'y a pas de piece)
+    char typePiece;     //Type = D(ame), P(ion) ou ' ' (s'il n'y a pas de piece)
+    char couleurCase;
+};
