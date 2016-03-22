e6e71399b1a779a306c19bc3616d472d52196df4
Sándor Koppándi <ifjsandor.koppandi@gmail.com>
2016-03-22 18:08:17 +0100

    Revert "Main"
    This reverts commit 851306f56f20bb553f59db9fcf2cbcbb3ebe17e5.

--- main.h
+++
@@ -1,6 +0,0 @@
-struct Piece
-{
-    char Position;  //Couleur = N ou B ou ' ' (s'il n'y a pas de piece)
-    char typePiece;     //Type = D(ame), P(ion) ou ' ' (s'il n'y a pas de piece)
-    char couleurCase;
-};
